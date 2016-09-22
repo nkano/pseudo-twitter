0941a572fbfeb8ba88d48af2824a5141414da9e9
nkano <xenosato@gmail.com>
2016-09-23 01:27:33 +0900

    added every file

---
+++ app/Test/Case/Model/UserTest.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+App::uses('AuthComponent', 'Controller/Component');
+
+class UserTest extends CakeTestCase {
+  public function testValidation(){
+    $this->User = ClassRegistry::init('User');
+    
+    $this->User->create(array(
+          'username' => 'testUser',
+          'password' => '',
+          'mail' => ''
+          
+        ));
+    $results = $this->User->invalidFields();
+
+    $this->assertEquals(array_key_exists('username', $results), false);
+    $this->assertEquals(array_key_exists('password', $results), true);
+    $this->assertEquals(array_key_exists('mail', $results), true);
+  }
+}
+?>
